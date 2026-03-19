4958f3d156ef325f1a621d6c9ebc44f786660b76
Calvin Kishore <calvin.k.kishore@gmail.com>
2026-03-20 00:21:22 +1100

    Updated Imports

--- document_loader.py
+++ document_loader.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import WebBaseLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_core.documents import Document
+from langchain_core.documents import Document #Had to change this line from before
 import os
 from typing import List, Union
 
