45deac1dc3cceb58699bfe005da167b884282e65
Calvin Kishore <calvin.k.kishore@gmail.com>
2026-03-26 23:57:24 +1100

    Added vector_store + Changed structure of requirments.txt

---
+++ vector_store.py
@@ -0,0 +1,33 @@
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_core.documents import Document
+from typing import List
+import os
+
+
+class VectorStore:
+    def __init__(self, model_name="nomic-embed-text", persist_dir="chroma_db"):
+        self.embeddings = OllamaEmbeddings(model=model_name)
+        self.persist_dir = persist_dir
+        self.store = None
+
+    def build(self, docs: List[Document]):
+        self.store = Chroma.from_documents(
+            documents=docs,
+            embedding=self.embeddings,
+            persist_directory=self.persist_dir
+        )
+        return self.store
+
+    def save(self):
+        pass
+
+    def load(self):
+        self.store = Chroma(
+            embedding_function=self.embeddings,
+            persist_directory=self.persist_dir
+        )
+        return self.store
+
+    def as_retriever(self, k=4):
+        return self.store.as_retriever(search_kwargs={"k": k})
