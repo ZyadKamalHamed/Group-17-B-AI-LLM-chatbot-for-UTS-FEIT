ed7eed5d99eaf5bbaa15995df02d1369a615ac07
ZyadKamalHamed <zyad2408@live.com.au>
2026-05-15 18:34:19 +1000

    Remove unused test files

--- test_search.py
+++
@@ -1,3 +0,0 @@
-from langchain_community.tools import DuckDuckGoSearchRun
-
-print(DuckDuckGoSearchRun().run("UTS FEIT undergraduate courses in 2026?"))
--- test_vector_store.py
+++
@@ -1,8 +0,0 @@
-from document_loader import DocumentLoader
-from vector_store import VectorStore
-
-loader = DocumentLoader()
-docs = loader.load_webpage("https://www.uts.edu.au/about/faculty-engineering-and-it")
-
-vs = VectorStore(model_name="nomic-embed-text")
-vs.build(docs)
